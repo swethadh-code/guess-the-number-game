c3d3ee22d51cdae71752a26e846ff6b51dfe6962
Swetha <swethadh8@gmail.com>
2026-08-02 16:14:34 +0530

    sign up

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 
 def sign_up():
+  print("Welcome to the sign-up page!")
+  print("password should contain at least 8 characters, including at least one uppercase letter, one lowercase letter, and one number.")
   username = input("Enter your username: ")
-  password = int(input("Enter your password: "))
-  if username.isalnum():
+  password = input("Enter your password: ")
+  if username.isalnum() and len(password) >= 8:
       print("Login successful!")
-      
   else:
      print("Invalid username or password.")
     
