7f54fdbbcc80d0e762bc5e4d48635c92a354aede
Swetha <swethadh8@gmail.com>
2026-08-02 15:37:18 +0530

    login

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-def login():
-    # Function to handle user login
-    username = input("Enter your username: ")
-    password = input("Enter your password: ")
+
+def sign_up():
+  username = input("Enter your username: ")
+  password = int(input("Enter your password: "))
+  if username.isalnum():
+      print("Login successful!")
+      
+  else:
+     print("Invalid username or password.")
     
-    # Here you would typically check the username and password against a database
-    if authenticate_user(username, password):
-        print("Login successful!")
-        return True
-    else:
-        print("Invalid username or password.")
-        return False
+sign_up()
